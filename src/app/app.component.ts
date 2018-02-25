b8627f591befb881e738c62df303287d9b207eef
Sergii Chervonyi <Bekyts@gmail.com>
2018-02-25 20:17:09 +0200

    add property example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,36 @@ export class AppComponent implements OnInit {
   title = 'app';
 
   ngOnInit(): void {
-    let g = new Person("Rick");
-    console.log(g.greet(undefined));
+    // let person = new Person("Rick");
+    // console.log(person.greet(undefined));
+
+    let greeter = new Greeter("Summer");
+    console.log(greeter.greet());
+  }
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+
+const formatMetadataKey = Symbol("format");
+
+function Format(formatString: string) {
+    return Reflect.metadata(formatMetadataKey, formatString);
+}
+
+function getFormat(target: any, propertyKey: string) {
+    return Reflect.getMetadata(formatMetadataKey, target, propertyKey);
+}
+
+class Greeter {
+  @Format("Hello, %s") greeting: string;
+
+  constructor(message: string) {
+      this.greeting = message;
+  }
+
+  greet() {
+      let formatString = getFormat(this, "greeting");
+      return formatString.replace("%s", this.greeting);
   }
 }
 
