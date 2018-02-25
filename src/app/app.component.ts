d529d78619f742f44a3db53a3cf432cc96b0d787
Serhii Chervonyi <serhii.chervonyi@dev-pro.net>
2018-02-25 19:04:48 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import "reflect-metadata";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+
+  ngOnInit(): void {
+    let g = new Greeter("Rick");
+    console.log(g.greet("Morti"));
+  }
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+
+const requiredMetadataKey = Symbol("required");
+
+function Required(target: Object, propertyKey: string | symbol, parameterIndex: number): void {
+  let existingRequiredParameters: number[] = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyKey) || [];
+  existingRequiredParameters.push(parameterIndex);
+  Reflect.defineMetadata(requiredMetadataKey, existingRequiredParameters, target, propertyKey);
+}
+
+function Validate(target: any, propertyName: string, descriptor: TypedPropertyDescriptor<any>): void {
+  let method = descriptor.value;
+  descriptor.value = function () {
+    let requiredParameters: number[] = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyName);
+    if (requiredParameters) {
+      for (let parameterIndex of requiredParameters) {
+        if (parameterIndex >= arguments.length || arguments[parameterIndex] === undefined) {
+          throw new Error("Missing required argument.");
+        }
+      }
+    }
+
+    return method.apply(this, arguments);
+  }
+}
+
+class Greeter {
+  greeting: string;
+
+  constructor(message: string) {
+    this.greeting = message;
+  }
+
+  @Validate
+  greet(@Required name: any) {
+    return "Hello " + name + ", " + this.greeting;
+  }
+}
